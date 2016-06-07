ee316f47f653d22158589ff46a1b9fe9c14d1293
Krukas <maikel_martens_1@hotmail.com>
2016-06-07 22:03:08 +0200

    [TASK] Change Markdown to reStructuredText and update setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,27 @@ try:
 except ImportError:
 	from distutils.core import setup
 
+def readme():
+	with open('README.rst') as f:
+		return f.read()
+
 setup(
 	name = 'Mage2Gen',
 	packages = ['mage2gen', 'mage2gen.snippets'],
 	package_data={'mage2gen': ['templates/*.tmpl']},
 	version = '0.3',
 	description = 'Magento 2 module generator',
+	long_description=readme(),
+	classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Code Generators',
+      ],
 	author = 'Maikel Martens',
 	author_email = 'maikel@martens.me',
+	license='GPL3',
 	url = 'https://github.com/krukas/Mage2Gen',
 	download_url = 'https://github.com/krukas/Mage2Gen/releases/tag/0.3',
 	keywords = ['Magento', 'Magento2', 'module', 'generator', 'mage2gen'],
-	classifiers = [],
 	install_requires=[],
 )
