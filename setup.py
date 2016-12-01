3ff85cd9f2b93d5a0d99aa0b1fc6839e817e1944
Krukas <maikel_martens_1@hotmail.com>
2016-12-01 17:46:42 +0100

    [TASK] Update version

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ def readme():
 setup(
 	name = 'Mage2Gen',
 	packages = ['mage2gen', 'mage2gen.snippets'],
-	package_data={'mage2gen': ['templates/*.tmpl', 'templates/payment/*.tmpl', 'templates/attributes/*.tmpl']},
+	package_data={'mage2gen': ['templates/*.tmpl', 'templates/payment/*.tmpl', 'templates/attributes/*.tmpl', 'licenses/*.txt']},
 	scripts=['bin/mage2gen'],
-	version = '0.7',
+	version = '0.8',
 	description = 'Magento 2 module generator',
 	long_description=readme(),
 	classifiers=[
@@ -24,7 +24,7 @@ setup(
 	author_email = 'maikel@martens.me',
 	license='GPL3',
 	url = 'https://github.com/krukas/Mage2Gen',
-	download_url = 'https://github.com/krukas/Mage2Gen/releases/tag/0.7',
+	download_url = 'https://github.com/krukas/Mage2Gen/releases/tag/0.8',
 	keywords = ['Magento', 'Magento2', 'module', 'generator', 'mage2gen'],
 	install_requires=[],
 )
