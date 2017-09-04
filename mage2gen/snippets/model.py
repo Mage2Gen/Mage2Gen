6b81b292065e15d67e83d0a2b57bbd574e233dfd
Krukas <maikel_martens_1@hotmail.com>
2017-09-04 19:28:07 +0200

    [TASK] Make text field default textarea

--- mage2gen/snippets/model.py
+++ mage2gen/snippets/model.py
@@ -90,7 +90,7 @@ class ModelSnippet(Snippet):
 			field_element_type = 'checkbox'
 		elif field_type == 'date' or field_type == 'timestamp':
 			field_element_type = 'date'
-		elif field_type == 'text' and extra_params.get('textarea'):
+		elif field_type == 'text':
 			field_element_type = 'textarea'
 		
 		install_class = Phpclass('Setup\\InstallSchema',implements=['InstallSchemaInterface'],dependencies=[
@@ -1380,12 +1380,5 @@ class ModelSnippet(Snippet):
 				yes_no=True, 
 				depend={'field_type': r'smallint|integer|bigint|float|decimal|numeric'}
 			),
-			SnippetParam(
-				name='textarea', 
-				yes_no=True, 
-				depend={'field_type': r'text'}
-			),
-
 		]
-
 
