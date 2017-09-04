691f5503fdc94f994c0b2f4e09022ffc817a53e8
Derrick Heesbeen <derrick@Derricks-MacBook-Pro.local>
2017-09-04 19:15:04 +0200

    [BUGFIX] removed unfinished snippet

--- mage2gen/snippets/__init__.py
+++ mage2gen/snippets/__init__.py
@@ -33,5 +33,4 @@ from .model import ModelSnippet
 from .api import ApiSnippet
 from .widget import WidgetSnippet
 from .cache import CacheSnippet
-from .uicomponent import UiComponentSnippet
 from .configurationtype import ConfigurationTypeSnippet
