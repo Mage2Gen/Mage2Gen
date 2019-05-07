4bf3fd65fdd4d10a8a08fb227d9f0f53333de4c4
Lewis Voncken <lewis@experius.nl>
2019-05-07 14:16:40 +0200

    [BUGFIX] [issue-271] Set correct source model classname

--- mage2gen/snippets/productattribute.py
+++ mage2gen/snippets/productattribute.py
@@ -98,13 +98,16 @@ class ProductAttributeSnippet(Snippet):
 		if not attribute_code:
 			attribute_code = attribute_label.lower().replace(' ','_')[:30]
 
+		split_attribute_code = attribute_code.split('_')
+		attribute_code_capitalized = ''.join(upperfirst(item) for item in split_attribute_code)
+
 		if source_model and frontend_input in ['multiselect', 'select']:
-			source_model = "\{}\{}\Model\Product\Attribute\Source\{}::class".format(self._module.package, self._module.name, upperfirst(attribute_code))
+			source_model = "\{}\{}\Model\Product\Attribute\Source\{}::class".format(self._module.package, self._module.name, attribute_code_capitalized)
 			options_array = []
 			for val in options:
 				options_array.append("['value' => '" + val.lower() + "', 'label' => __('" + val + "')]")
 			options_php_array = '[\n' + ',\n'.join(x.strip() for x in options_array) + '\n]'
-			self.add_source_model(attribute_code, options_php_array, extra_params.get('used_in_product_listing', False))
+			self.add_source_model(attribute_code_capitalized, options_php_array, extra_params.get('used_in_product_listing', False))
 			options_php_array_string = "''"
 		else:
 			source_model = "''"
@@ -113,6 +116,15 @@ class ProductAttributeSnippet(Snippet):
 
 		with open(templatePath, 'rb') as tmpl:
 			template = tmpl.read().decode('utf-8')
+
+		is_swatch_option = frontend_input == 'swatch_visual' or frontend_input == 'swatch_text'
+
+		if frontend_input == 'swatch_visual':
+			options_php_array_string = "['values' => ['Black' => '#000000', 'White' => '#ffffff']]"
+		elif frontend_input == 'swatch_text' :
+			options_php_array_string = "['values' => ['Sample' => 'Sample']]"
+		else:
+			options_php_array_string = options_php_array_string
 
 		methodBody = template.format(
 			attribute_code=attribute_code,
@@ -255,8 +267,8 @@ $productSetup->installEntities();""".format(class_name=product_data.class_name),
 		])
 		self.add_xml('etc/module.xml', etc_module)
 
-	def add_source_model(self, attribute_code, options_php_array_string, used_in_product_listing):
-		source_model = Phpclass('Model\\Product\\Attribute\Source\\{}'.format(upperfirst(attribute_code)),
+	def add_source_model(self, attribute_code_capitalized, options_php_array_string, used_in_product_listing):
+		source_model = Phpclass('Model\\Product\\Attribute\Source\\{}'.format(upperfirst(attribute_code_capitalized)),
 			extends='\\Magento\\Eav\\Model\\Entity\\Attribute\\Source\\AbstractSource')
 
 		source_model.add_method(Phpmethod(
