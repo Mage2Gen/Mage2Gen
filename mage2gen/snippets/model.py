cf24d9ecf1beee717469015b5918dd2213d8ff41
Boris van Katwijk <borisvankatwijk@gmail.com>
2021-11-24 16:23:39 +0100

    [BUGFIX][BACI-541] The xml preference declared for Api\Data\[model]Interface isn't routing to the correct class (still has the \Data\ folder incorporated). Resolved this for the di.xml generation in the model.py snippet.

--- mage2gen/snippets/model.py
+++ mage2gen/snippets/model.py
@@ -520,18 +520,18 @@ class ModelSnippet(Snippet):
 
 		# Create di.xml preferences
 		self.add_xml('etc/di.xml', Xmlnode('config', attributes={'xsi:noNamespaceSchemaLocation': "urn:magento:framework:ObjectManager/etc/config.xsd"}, nodes=[
-		    Xmlnode('preference', attributes={
-		        'for': "{}\\{}\\Api\\{}RepositoryInterface".format(self._module.package, self._module.name, model_name_capitalized),
-		        'type': model_repository_class.class_namespace
-		    }),
-		    Xmlnode('preference', attributes={
-		        'for': "{}\\{}\\Api\\Data\\{}Interface".format(self._module.package, self._module.name, model_name_capitalized),
-		        'type': "{}\\{}\\Model\\Data\\{}".format(self._module.package, self._module.name, model_name_capitalized)
-		    }),
-		    Xmlnode('preference', attributes={
-		        'for': "{}\\{}\\Api\\Data\\{}SearchResultsInterface".format(self._module.package, self._module.name, model_name_capitalized),
-		        'type': 'Magento\Framework\Api\SearchResults'
-		    })
+			Xmlnode('preference', attributes={
+				'for': "{}\\{}\\Api\\{}RepositoryInterface".format(self._module.package, self._module.name, model_name_capitalized),
+				'type': model_repository_class.class_namespace
+			}),
+			Xmlnode('preference', attributes={
+				'for': "{}\\{}\\Api\\Data\\{}Interface".format(self._module.package, self._module.name, model_name_capitalized),
+				'type': "{}\\{}\\Model\\{}".format(self._module.package, self._module.name, model_name_capitalized)
+			}),
+			Xmlnode('preference', attributes={
+				'for': "{}\\{}\\Api\\Data\\{}SearchResultsInterface".format(self._module.package, self._module.name, model_name_capitalized),
+				'type': 'Magento\Framework\Api\SearchResults'
+			})
 		]))
 
 		# add grid
